c61ec11624c8d350a769b18fb6bcf8770eead235
erenduygu <erenduygu33@gmail.com>
2021-01-27 12:30:06 +0300

    new println "new hello" added at the end

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         // We are good at IntelliJ
+
+        System.out.println("new hello");
     }
 }
